43f8c8d850663a4b4689d99e2a46be671e09b489
ciprianciuhan <cipriancucuciuhan@gmail.com>
2020-07-07 23:57:37 +0300

    i might be on to something

--- src/home_appraiser/MainClass.java
+++ src/home_appraiser/MainClass.java
@@ -15,7 +15,7 @@ import org.jsoup.select.Elements;
 public class MainClass {
 
 	public static void main(String[] args) {
-		/*
+		
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -40,29 +40,35 @@ public class MainClass {
 		int sup_min = (int) property.getSuprafata() - 5;
 		int sup_max = (int) property.getSuprafata() + 5;
 		
-		String tip_casa_url = null;
+		String tip_casa_url = "";
 		
-		if (property.getType() == "casa") {
+		if (property.getType().charAt(0) == 'c') {
 			tip_casa_url = "case";
 		} else {
 			tip_casa_url = "apartamente-garsoniere";
 		}
-		*/
+		
+		int nr_camere = 0;
+		
+		if (property.getNrCamere() > 4) {
+			nr_camere = 4;
+		} else nr_camere = property.getNrCamere();
+		
+		System.out.println(tip_casa_url);
+		
 		
 		ArrayList <String> vector_preturi = new ArrayList<String>();
 		int i = 0;
 		
 		try {
-			/*
-			Document doc = Jsoup.connect("https://www.olx.ro/imobiliare/" + tip_casa_url + "-de-vanzare/" + property.getNrCamere() 
-			+ "/" + property.getOras().toLowerCase() + "/?search%5Bfilter_float_m%3Afrom%5D=" + sup_min 
-			+ "&search%5Bfilter_float_m%3Ato%5D=" + sup_max).userAgent("Mozilla/17.0").get();
-			*/
 			
-			Document doc = Jsoup.connect("https://www.olx.ro/imobiliare/apartamente-garsoniere-de-vanzare/2-camere/bucuresti/?search%5Bfilter_float_m%3Afrom%5D=35&search%5Bfilter_float_m%3Ato%5D=45").get();
+			Document doc = Jsoup.connect("https://www.olx.ro/imobiliare/" + tip_casa_url + "-de-vanzare/" + nr_camere 
+			+ "-camere/" + property.getOras().toLowerCase() + "/?search%5Bfilter_float_m%3Afrom%5D=" + sup_min 
+			+ "&search%5Bfilter_float_m%3Ato%5D=" + sup_max).get();
+			
+			
+			//Document doc = Jsoup.connect("https://www.olx.ro/imobiliare/apartamente-garsoniere-de-vanzare/2-camere/bucuresti/?search%5Bfilter_float_m%3Afrom%5D=35&search%5Bfilter_float_m%3Ato%5D=45&page=98").get();
 			Elements temp = doc.select("p.price");
-			
-			
 			
 			//ArrayList <String> vector_preturi = new ArrayList<String>();
 			
@@ -79,13 +85,17 @@ public class MainClass {
 			
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		for (int x = 0; x < i; x++) {
 			System.out.println(vector_preturi.get(x));
 		}
+		
+		//trebuie sa fac un for sau ceva care sa ma duca prin toate paginile fara sa imi dea eroare 
+		//ca sa pot sa adun cat mai multe preturi
+		//dupa trebuie sa convertesc preturile in int si sa sortez vectorul, apoi sa afisez min, max si 
+		//sa fac media pentru apartament, cumva printr-o diferentiere a cartierelor
 		
 		
 		
